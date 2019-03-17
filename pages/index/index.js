6a66a4fb04a7ec1ed7b2651a891a8d5001e83faf
jia <15901226963@163.com>
2019-03-17 18:48:06 +0800

    实现朗读与copy

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,16 @@
 //index.js
 import {translate} from '../../utils/api.js'
 //获取应用实例
-const app = getApp()
-
+const app = getApp()    
 Page({
   data: {
     hideClearIcon:true,
     curLang:{},
     query:'',
-    result:[]
+    result:[],
+    hornActive:false,
+    starActive:false,
+    copyActive:false
   },
   //事件处理函数
   onLoad: function (e) {
@@ -17,9 +19,6 @@ Page({
       this.setData({ curLang: app.globalData.curLang })
       this.onConfirm()
     }
-  },
-  onblur:function(e){
-    console.log(this.data.query)
   },
   onInput: function (e) {
     this.setData({ 'query': e.detail.value })
@@ -30,7 +29,7 @@ Page({
     }
   },
   onTapclose:function(){
-    this.setData({ 'query': '',hideClearIcon:true })
+    this.setData({ 'query': '',hideClearIcon:true,'result':[] })
   },
   onShow: function () {
     if (this.data.curLang.lang !== app.globalData.curLang.lang) {
@@ -41,11 +40,40 @@ Page({
   onConfirm: function () {
     if (!this.data.query) return
     translate(this.data.query, { from: 'auto', to: this.data.curLang.lang }).then(res => {
+      this.setData({ copyActive: false })
       this.setData({ 'result': res.trans_result })
       let history = wx.getStorageSync('history') || []
       history.unshift({ query: this.data.query, result: res.trans_result[0].dst, curLang: this.data.curLang })
       history.length = history.length > 10 ? 10 : history.length
       wx.setStorageSync('history', history)
+    })
+  },
+  onTapRead:function(){
+    const innerAudioContext = wx.createInnerAudioContext()
+    console.log(this.data.result[0].dst)
+    innerAudioContext.autoplay=true
+    innerAudioContext.src = `https://tsn.baidu.com/text2audio?tex=${this.data.result[0].dst}&tok=24.e4b65633c202b35fa774641cd2b8a71b.2592000.1555381472.282335-15773870&cuid='abcdxxx'&ctp=1&lan=zh`
+    innerAudioContext.onPlay(()=>{
+      console.log('播放开始')
+      this.setData({ hornActive: true })
+    })
+    innerAudioContext.onEnded(() => {
+      console.log('播放结束')
+      this.setData({ hornActive: false })
+    })
+  },
+  onstar:function(){
+    this.setData({ starActive: !this.data.starActive })
+  },
+  oncopy:function(){
+    this.setData({ copyActive: true })
+    wx.setClipboardData({
+      data: String(this.data.result),
+      success: res => {
+        wx.showToast({
+          title: '复制成功',
+        })
+      }
     })
   }
 })
